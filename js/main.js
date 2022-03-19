a59cf5f4f8604fd079444314452b81df87032f3e
Yurii Kosach <81816470+YurKo22@users.noreply.github.com>
2022-03-19 22:28:40 +0200

    2.1.2

--- js/main.js
+++ js/main.js
@@ -5,8 +5,16 @@ $(function() {
         nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt=""></button>',
     });
 
-    $('.menu__btn').on('click', function() {
-        $('.menu__list').slideToggle();
+    //  $('').on('click', function() {
+    //      $('.menu__list').slideToggle();
+    //  });
+
+    //  $(".menu__btn").on("click", function() {
+    //      $(".menu__list").slideToggle("slow");
+    //  });
+
+    $(".menu__btn").click(function() {
+        $(".menu__list").slideToggle();
     });
 
 });
